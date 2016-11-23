1ed25bebc1c84e43e141fa9c2751652d0053f469
kfrn <knfrances@gmail.com>
2016-11-23 14:32:42 +1300

    Updates

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const { createStore } = require('redux')
+const html = require('yo-yo')
 const { update } = require('yo-yo')
 
 const Home = require('./templates')
+const Cart = require('./templates/cart')
 const Searched = require('./templates/searched')
 const reducer = require('./reducer')
 
+const sheetRouter = require('sheet-router')
+
 const initState = {
+  route: '/',
   products: [
-    {name: 'Doctor Strange', poster: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/xfWac8MTYDxujaxgPVcRD9yZaul.jpg', price: 25, deal: true},
-    {name: 'Iron Man', poster: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/s2IG9qXfhJYxIttKyroYFBsHwzQ.jpg', price: 25, deal: true},
-    {name: 'Gladiator', poster: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/6WBIzCgmDCYrqh64yDREGeDk9d3.jpg', price: 25, deal: false}
+    {name: 'Doctor Strange', poster: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/xfWac8MTYDxujaxgPVcRD9yZaul.jpg', price: 25, stock: 13, deal: true},
+    {name: 'Iron Man', poster: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/s2IG9qXfhJYxIttKyroYFBsHwzQ.jpg', price: 20, stock: 7, deal: true},
+    {name: 'Gladiator', poster: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/6WBIzCgmDCYrqh64yDREGeDk9d3.jpg', price: 25, stock: 10, deal: false}
   ],
   searched: [],
   cart: [],
@@ -17,33 +22,35 @@ const initState = {
 }
 const { dispatch, subscribe, getState } = createStore(reducer, initState)
 
-const goHome = () => {
-  // console.log('homeView')
+const goCart = () => {
   const state = getState()
-  const newView = Home(state, dispatch)
+  const newView = Cart(state, dispatch)
   update(initView, newView)
 }
 
-const goCart = () => {
-  console.log('cart');
-}
-
 const showSearched = () => {
-  // console.log('searchedView')
+  // console.log('render searched products')
   const state = getState()
-  if(state.searched.length > 0){
+  if(state.searched.length != 0){
     const newView = Searched(state, dispatch)
     update(initView, newView)
   }
 }
 
-const main = document.querySelector('main')
-const initView = main.appendChild(Home(initState, dispatch))
+const router = sheetRouter({ default: '/404' }, [
+  ['/', (params) => Home(getState(), dispatch)],
+  ['/cart', (params) => Cart(getState(), dispatch)],
+  ['/404', (params) => html`<div>Oh no, path not found!</div>`]
+])
 
-subscribe(goHome)
-subscribe(showSearched)
-
-module.exports = {
-  goHome,
-  goCart
+const view = router(getState().route)
+const link = () => {
+  const newView = router(getState().route)
+  update(initView, newView)
 }
+
+const main = document.querySelector('main')
+const initView = main.appendChild(view)
+
+subscribe(link)
+subscribe(showSearched)
